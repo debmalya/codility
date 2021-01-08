3ee5c1b8dbda755dfcc8f31720dc7f1bd929ac3c
Debmalya Jash <debmalya.jash@gmail.com>
2021-01-09 01:40:13 +0800

    Demo

--- src/main/java/org/deb/Demo.java
+++ src/main/java/org/deb/Demo.java
@@ -5,18 +5,17 @@ import java.util.Arrays;
 public class Demo {
     public int solution(int[] A) {
         // write your code in Java SE 8
+
         Arrays.sort(A);
-        int a = 1;
-        for (int i = 1; i < A[A.length-1]; i++){
-            if (Arrays.binarySearch(A,i)<0){
+
+        for (int i = 1; i < A[A.length - 1]; i++) {
+            if (Arrays.binarySearch(A, i) < 0) {
                 return i;
             }
         }
-        if (A[A.length-1]>0){
-            return A[A.length -1]+1;
-        }
-        return a;
+
+        return Math.max(1, A[A.length - 1] + 1);
     }
 
-   
+
 }
